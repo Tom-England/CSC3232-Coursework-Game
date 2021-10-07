4ebc7835d39e6d8e3ad42eb2b0e83290aaed42b5
Tom-England <tomengland6679@gmail.com>
2021-10-07 15:13:59 +0100

    Created testing level and begun work on player controller

---
+++ Assets/Scripts/ScrPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPlayerController : MonoBehaviour
+{
+
+    float speed = 10f;
+    public Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRB.AddForce(Vector3.forward * Input.GetAxis("Vertical") * speed, ForceMode.Force);
+        playerRB.AddForce(Vector3.right * Input.GetAxis("Horizontal") * speed, ForceMode.Force);
+    }
+}
