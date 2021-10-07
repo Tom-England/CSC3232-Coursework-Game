2497df15039c63d551634beefa8dac5dd9bbd69a
Tom-England <tomengland6679@gmail.com>
2021-10-07 15:42:42 +0100

    Added Jumping to ScrPlayerController

--- Assets/Scripts/ScrPlayerController.cs
+++ Assets/Scripts/ScrPlayerController.cs
@@ -6,7 +6,18 @@ public class ScrPlayerController : MonoBehaviour
 {
 
     float speed = 10f;
+    public float jump_force = 100f;
+    bool in_air = false;
     public Rigidbody playerRB;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Floor")
+        {
+            in_air = false;
+        }
+    }
 
     Vector3 GetPos()
     {
@@ -18,8 +29,9 @@ public class ScrPlayerController : MonoBehaviour
     {
         float y_axis = Input.GetAxis("Vertical");
         float x_axis = Input.GetAxis("Horizontal");
+        bool jump_flag = Input.GetButtonDown("Jump");
 
-        if (y_axis == 0 && x_axis == 0)
+        if (y_axis == 0 && x_axis == 0 && !jump_flag)
         {
             // No Movement
             return false;
@@ -29,6 +41,13 @@ public class ScrPlayerController : MonoBehaviour
             // Movement
             playerRB.AddForce(Vector3.forward * y_axis * speed, ForceMode.Force);
             playerRB.AddForce(Vector3.right * x_axis * speed, ForceMode.Force);
+
+            // Handle Jump
+            if (jump_flag && !in_air)
+            {
+                playerRB.AddForce(Vector3.up * jump_force, ForceMode.Force);
+                in_air = true;
+            }
             return true;
         }
     }
