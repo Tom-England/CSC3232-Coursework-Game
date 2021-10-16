c9504b7fe93ffc21fe9e8f3754d981eb6fd57237
Tom-England <tomengland6679@gmail.com>
2021-10-16 14:22:22 +0100

    Worked on first level

---
+++ Assets/Scripts/ScrObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrObject : MonoBehaviour
+{
+    Vector3 CalculateDirection(Vector3 other)
+    {
+        Vector3 dir = other - transform.position;
+        Vector3 norm_dir = dir.normalized;
+        return norm_dir;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Attack")
+        {
+            Debug.Log("Hit");
+            Vector3 dir = CalculateDirection(other.transform.position);
+            GetComponent<Rigidbody>().AddForce(dir * -500, ForceMode.Force);
+        }
+    }
+}
