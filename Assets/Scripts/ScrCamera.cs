a6136fa8f9c538e0229af5fe5b09001489a456c3
Tom-England <tomengland6679@gmail.com>
2021-11-01 16:58:34 +0100

    Camera Changes

--- Assets/Scripts/ScrCamera.cs
+++ Assets/Scripts/ScrCamera.cs
@@ -6,9 +6,13 @@ public class ScrCamera : MonoBehaviour
 {
 
     public GameObject player;
+    Vector3 new_pos;
     // Update is called once per frame
     void Update()
     {
+
+        new_pos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = new_pos;
         transform.LookAt(player.transform.position);
     }
 }
